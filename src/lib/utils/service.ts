97792ca2ff0d61681d8b49032f72557e9289b6be
John Kiernan <jkiernan12@yahoo.com>
2024-04-27 07:57:43 -0400

    fix: decode html char codes from BGG API

--- src/lib/utils/service.ts
+++ src/lib/utils/service.ts
@@ -1,10 +1,11 @@
 import { bgg } from '../utils/constants';
 import { XMLParser } from 'fast-xml-parser';
+import {decode} from 'html-entities';
 
 export interface Game {
   description: string;
   name: {
-    type: string;
+    type?: string;
     value: string;
   }[];
   image: string;
@@ -27,13 +28,24 @@ export interface Game {
 
 export const getGames = async (username: string) : Promise<Game[]> => {
   const parser = new XMLParser({ignoreAttributes: false});
+  // get user game IDs
   const res = await fetch(bgg + `collection?username=${username}`).then(response => response.text());
   const gameIds = parser.parse(res).items.item.map((game: any) => game['@_objectid']);
   const gameIdString = gameIds.join(',');
-  const gamesXml = await fetch(bgg + 'thing?id=' + gameIdString + '&stats=1').then(response => response.text());
-  const gamesParser = new XMLParser({attributeNamePrefix: '', ignoreAttributes: false, isArray: (name) => name === 'name'});
-  const games = gamesParser.parse(gamesXml).items.item;
-  console.log(games[0])
 
-  return games;
+  // get game data
+  const gamesXml = await fetch(bgg + 'thing?id=' + gameIdString + '&stats=1').then(response => response.text()).then(xml => decode(xml));
+  const gamesParser = new XMLParser({attributeNamePrefix: '', ignoreAttributes: false, isArray: (name) => name === 'name'});
+  const games: Game[] = gamesParser.parse(gamesXml).items.item;
+
+  // decode html char codes in text
+  const decodedGames = games.map(game => {
+    return {
+      ...game,
+      title: [{value: decode(game.name[0].value)}], // only decode title we're using
+      description: decode(game.description),
+    }
+  })
+
+  return decodedGames;
 }
