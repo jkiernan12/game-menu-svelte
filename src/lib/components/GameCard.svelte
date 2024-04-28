b3197e99cb96eecf398703aa8e8ba2aa4d83c409
John Kiernan <jkiernan12@yahoo.com>
2024-04-28 14:22:43 -0400

    feat: add expandable description and visual improvements

---
+++ src/lib/components/GameCard.svelte
@@ -0,0 +1,24 @@
+<script lang='ts'>
+	import type { Game } from "$lib/utils/service";
+
+  export let game: Game;
+  let expanded = false;
+</script>
+
+<div class="card p-4 w-full">
+  <header class="card-header">
+    <img alt={game.name[0].value + ' cover image'} src={game.image} />
+  </header>
+  <section class={`p-4`}>
+    <h3 class="h3">{game.name[0].value}</h3>
+    <p class={expanded ? null : "line-clamp-6"}>
+      {game.description}
+    </p>
+    <button on:click={() => expanded = !expanded} class="font-bold underline">Read {expanded ? 'less' : 'more'}</button>
+    <div class='py-4'>
+      <p><b>Players</b>: {game.minplayers.value}-{game.maxplayers.value}</p>
+      <p><b>Playtme</b>: {game.minplaytime.value}-{game.maxplaytime.value} mins</p>
+      <p><b>Weight</b>: {game.statistics.ratings.averageweight.value}</p>
+    </div>
+  </section>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,7 @@
   import { getGames } from '$lib/utils/service';
   import { type Game } from '$lib/utils/service';
   import DoubleRangeSlider from '../lib/components/DoubleRangeSlider.svelte';
+	import GameCard from '$lib/components/GameCard.svelte';
 
   let filteredGames: Game[];
   let games: Game[] = [];
@@ -42,9 +43,8 @@
 
 <div class="container h-full mx-auto flex justify-center items-center">
 	<div class="space-y-5 w-full p-4">
-		<h1 class="h1">Game Menu</h1>
-		<p>Start by exploring:</p>
-    <section class="grid grid-cols-1 md:grid-cols-3 gap-8">
+		<h1 class="h1 text-center">Game Menu</h1>
+    <section class="px-9 grid grid-cols-1 md:grid-cols-3 md:px-0 gap-8">
 
       <label class="label">
         <span>Players</span>
@@ -71,22 +71,11 @@
         </div>
       </div>
     </section>
-    <section class="grid grid-cols-1 md:grid-cols-3 gap-8">
+    <section class="grid grid-cols-1 md:grid-cols-3 sm:grid-cols-2 gap-8">
 
       {#if games}
       {#each filteredGames as game}
-      <div class="card p-4 w-full">
-        <header class="card-header">
-          <img alt={game.name[0].value + ' cover image'} src={game.image} />
-        </header>
-        <section class={`p-4`}>
-          <h3 class="h3">{game.name[0].value}</h3>
-          <p class="line-clamp-6">{game.description}</p>
-          <p><b>Players</b>: {game.minplayers.value}-{game.maxplayers.value}</p>
-          <p><b>Playtme</b>: {game.minplaytime.value}-{game.maxplaytime.value} mins</p>
-          <p><b>Weight</b>: {game.statistics.ratings.averageweight.value}</p>
-        </section>
-      </div>
+      <GameCard game={game} />
       {/each}
       {:else}
       <p>Loading...</p>
